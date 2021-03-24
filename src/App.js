f6eecc26213720fffb9029c9786c3cd72d38cf66
Mohamed Fawzy <mofawzy1996@gmail.com>
2021-03-24 19:40:30 +0200

    Update App Component, index

--- src/App.js
+++ src/App.js
@@ -3,24 +3,14 @@ import "./App.css";
 import Pet from "./components/Pet";
 
 function App() {
-  return React.createElement("div", { id: "something-important" }, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      name: "Doink",
-      animal: "Cat",
-      breed: "Mixed",
-    }),
-  ]);
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="Cat" breed="Mixed" />
+    </div>
+  );
 }
 
 export default App;
