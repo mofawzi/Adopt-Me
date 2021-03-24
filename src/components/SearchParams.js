e1a37bcdf68b0b6d657e5a503eddeed6fe526d05
Mohamed Fawzy <mofawzy1996@gmail.com>
2021-03-24 22:25:26 +0200

    Update SearchParams Component

--- src/components/SearchParams.js
+++ src/components/SearchParams.js
@@ -1,15 +1,24 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Alexandria, Egypt.");
 
   const [breeds, setBreeds] = useState([]);
-
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
 
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+
+    // Get breeds from API
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedNames = breeds.map(({ name }) => name);
+      setBreeds(breedNames);
+    }, console.error);
+  }, [animal, setBreed, setBreeds]);
 
   return (
     <div className="search-params">
