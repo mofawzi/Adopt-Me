a66ab5edabb3ef5619d60e5a4d7517ddd0c275ef
Mohamed <Mohamed Fawzy>
2021-03-27 23:09:34 +0200

    Update App Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
 import "./App.css";
 import SearchParams from "./components/SearchParams";
-import { Router } from "@reach/router";
+import { Router, Link } from "@reach/router";
+import Details from "./components/Details";
 
 function App() {
   return (
     <div>
-      <h1 id="something-important">Adopt Me!</h1>
-      <SearchParams />
+      <header>
+        <Link to="/">Adopt Me!</Link>
+      </header>
+
+      <Router>
+        <SearchParams path="/" />
+        <Details path="/details/:id" />
+      </Router>
     </div>
   );
 }
