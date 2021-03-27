0ec3c80e926d3db99034c7db0206d1a6b4acec6f
Mohamed <Mohamed Fawzy>
2021-03-27 21:27:37 +0200

    Add From Submition

--- src/components/SearchParams.js
+++ src/components/SearchParams.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
+import Results from "./Results";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Alexandria, Egypt.");
@@ -8,6 +9,19 @@ const SearchParams = () => {
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
   const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+
+  const [pets, setPets] = useState([]);
+
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal,
+    });
+
+    setPets(animals || []);
+    console.log(pets);
+  }
 
   useEffect(() => {
     setBreeds([]);
@@ -22,7 +36,12 @@ const SearchParams = () => {
 
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location:
           <input
@@ -38,6 +57,8 @@ const SearchParams = () => {
 
         <button>Submit</button>
       </form>
+
+      <Results pets={pets} />
     </div>
   );
 };
