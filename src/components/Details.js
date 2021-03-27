1452f4db176ea44e9d99f428388eaa1be522a8fc
Mohamed <Mohamed Fawzy>
2021-03-28 00:08:00 +0200

    Update Details Component

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import pet from "@frontendmasters/pet";
 
-const Details = () => {
-  return <div>Hello from details</div>;
+const Details = (props) => {
+  const [animalState, setAnimalState] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    pet.animal(props.id).then(({ animal }) => {
+      setAnimalState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.breeds.primary,
+      });
+      setLoading(false);
+    }, console.error);
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  const { animal, breed, location, description, name } = animalState;
+
+  return (
+    <div className="details">
+      <div>
+        <h1>{name}</h1>
+        <h2>{`${animal} - ${breed} - ${location}`}</h2>
+        <button>Adopt {name}</button>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Details;
