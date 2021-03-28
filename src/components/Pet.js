55a99f95cfca2150f8096c122cc211ca1c6bedd1
Mohamed Fawzy <mofawzy1996@gmail.com>
2021-03-28 17:31:31 +0200

    Update Pet Component

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,3 +1,5 @@
+import { Link } from "@reach/router";
+
 export default function Pet({ name, animal, breed, media, location, id }) {
   // Default image
   let hero = "http://placecorgi.com/300/300";
@@ -6,7 +8,7 @@ export default function Pet({ name, animal, breed, media, location, id }) {
     hero = media[0].small;
   }
   return (
-    <a href={`/details/${id}`} className="pet">
+    <Link to={`/details/${id}`} className="pet">
       <div className="image-container">
         <img src={hero} alt={name} />
       </div>
@@ -15,6 +17,6 @@ export default function Pet({ name, animal, breed, media, location, id }) {
         <h1>{name}</h1>
         <h2>{`${animal} - ${breed} - ${location}`}</h2>
       </div>
-    </a>
+    </Link>
   );
 }
